4227f9ada87983ee65cb90571b9f30e38d4c78e1
Sean Lao <slao258@gmail.com>
2013-11-02 23:57:22 -0700

    updates Meetup Model to more closely follow meetup specs

--- src/com/codepath/apps/locateme/models/Meetup.java
+++ src/com/codepath/apps/locateme/models/Meetup.java
@@ -1,21 +1,38 @@
 package com.codepath.apps.locateme.models;
 
 import java.util.Date;
-import java.util.List;
 
 import android.location.Location;
 
 import com.activeandroid.Model;
 
 public class Meetup extends Model {
-	private enum Status {
-		ACTIVE, INACTIVE
+
+	private String name;
+	private Location location;
+	private Date timestamp;
+
+	public String getName() {
+		return name;
 	}
 
-	private int id;
-	private String name;
-	private Status status;
-	private Location location;
-	private List<User> users;
-	private Date timestamp;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
 }
---
+++ src/com/codepath/apps/locateme/models/UserMeetupState.java
@@ -0,0 +1,103 @@
+package com.codepath.apps.locateme.models;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import com.activeandroid.Model;
+
+public class UserMeetupState extends Model {
+
+	// MOCK
+	private static final Random RANDOM = new Random();
+	private static final List<Status> STATUS_VALUES = Collections.unmodifiableList(Arrays.asList(Status.values()));
+	private static final int STATUS_SIZE = STATUS_VALUES.size();
+	private static final List<Transport> TRANSPORT_VALUES = Collections.unmodifiableList(Arrays.asList(Transport.values()));
+	private static final int TRANSPORT_SIZE = TRANSPORT_VALUES.size();
+
+	public enum Status {
+		PENDING, ACTIVE, DECLINED, ARRIVED, CANCELLED, DELETED
+	}
+	public enum Transport {
+		CAR, PUBLIC, WALK
+	}
+	public static String getStatusString(Status status) {
+		String val = null;
+		switch (status) {
+		case PENDING:
+			val = "Pending";
+			break;
+		case ACTIVE:
+			val = "Active";
+			break;
+		case DECLINED:
+			val = "Declined";
+			break;
+		case ARRIVED:
+			val = "Arrived";
+			break;
+		case CANCELLED:
+			val = "Cancelled";
+			break;
+		case DELETED:
+			val = "Deleted";
+			break;
+		default:
+			val = "Unknown";
+			break;
+		}
+		return val;
+	}
+
+	private long userId;
+	private long meetupId;
+	private Status status;
+	private Transport transport;
+
+	public UserMeetupState() {
+		super();
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public long getMeetupId() {
+		return meetupId;
+	}
+
+	public void setMeetupId(long meetupId) {
+		this.meetupId = meetupId;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public Transport getTransport() {
+		return transport;
+	}
+	public void setTransport(Transport transport) {
+		this.transport = transport;
+	}
+
+	public static UserMeetupState byIds(long userId, long meetupId) {
+		// MOCK
+		UserMeetupState state = new UserMeetupState();
+		state.setUserId(userId);
+		state.setMeetupId(meetupId);
+		state.setStatus(STATUS_VALUES.get(RANDOM.nextInt(STATUS_SIZE)));
+		state.setTransport(TRANSPORT_VALUES.get(RANDOM.nextInt(TRANSPORT_SIZE)));
+		return state;
+
+		//return new Select().from(SampleModel.class).where("meetupId = ? and userId = ?", meetupId, userId).executeSingle();
+	}
+
+}
