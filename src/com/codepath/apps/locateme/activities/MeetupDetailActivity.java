ec2dc455943038532b818696a1f0888a0e3452b5
Sean Lao <slao258@gmail.com>
2013-11-03 22:23:34 -0800

    60038806: update layout for creating a meetup

--- src/com/codepath/apps/locateme/activities/MeetupDetailActivity.java
+++ src/com/codepath/apps/locateme/activities/MeetupDetailActivity.java
@@ -1,26 +1,34 @@
 package com.codepath.apps.locateme.activities;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.codepath.apps.locateme.R;
+import com.codepath.apps.locateme.models.Meetup;
 
 public class MeetupDetailActivity extends Activity {
+
+	EditText etDate;
+	EditText etTime;
+	Calendar myCalendar = Calendar.getInstance();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_meetup_detail);
 		setupViews();
-	}
-
-	private void setupViews() {
-
-		//		new DatePickerDialog(MeetupDetailActivity.this, date, myCalendar
-		//				.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-		//				myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-		//
 	}
 
 	@Override
@@ -30,4 +38,53 @@ public class MeetupDetailActivity extends Activity {
 		return true;
 	}
 
+	private void setupViews() {
+		// set up date picker
+		final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+			@Override
+			public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+				myCalendar.set(Calendar.YEAR, year);
+				myCalendar.set(Calendar.MONTH, monthOfYear);
+				myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+				SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy", Locale.US);
+				etDate.setText(sdf.format(myCalendar.getTime()));
+			}
+		};
+		etDate = (EditText) findViewById(R.id.etDate);
+		etDate.setOnFocusChangeListener(new OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					new DatePickerDialog(MeetupDetailActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar
+							.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+				}
+			}
+		});
+
+		// set up time picker
+		final TimePickerDialog.OnTimeSetListener time = new TimePickerDialog.OnTimeSetListener() {
+			@Override
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				myCalendar.set(Calendar.MINUTE, minute);
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.US);
+				etTime.setText(sdf.format(myCalendar.getTime()));
+			}
+		};
+		etTime = (EditText) findViewById(R.id.etTime);
+		etTime.setOnFocusChangeListener(new OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					new TimePickerDialog(MeetupDetailActivity.this, time, myCalendar.get(Calendar.HOUR_OF_DAY),
+							myCalendar.get(Calendar.MINUTE), true).show();
+				}
+			}
+		});
+	}
+
+	public void onSave(View v) {
+		new Meetup();
+		finish();
+	}
 }
