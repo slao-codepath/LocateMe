bc7382af4f1729b740edbd561edcdddd608b6e20
Sean Lao <slao258@gmail.com>
2013-11-21 08:00:38 -0800

    Initialize Parse

--- src/com/codepath/apps/locateme/LocateMeClientApp.java
+++ src/com/codepath/apps/locateme/LocateMeClientApp.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.parse.Parse;
 
 /*
  * This is the Android application itself and is used to configure various settings
@@ -13,26 +14,28 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
  * 
  *     RestClient client = RestClientApp.getRestClient();
  *     // use client to send requests to API
- *     
+ * 
  */
 public class LocateMeClientApp extends com.activeandroid.app.Application {
-	private static Context context;
-	
+    private static Context context;
+
     @Override
     public void onCreate() {
         super.onCreate();
         LocateMeClientApp.context = this;
-        
+
         // Create global configuration and initialize ImageLoader with this configuration
         DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
-        		cacheInMemory().cacheOnDisc().build();
+                cacheInMemory().cacheOnDisc().build();
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
-            .defaultDisplayImageOptions(defaultOptions)
-            .build();
+        .defaultDisplayImageOptions(defaultOptions)
+        .build();
         ImageLoader.getInstance().init(config);
+
+        Parse.initialize(this, getString(R.string.parse_appId), getString(R.string.parse_clientKey));
     }
-    
+
     public static LocateMeClient getRestClient() {
-    	return (LocateMeClient) LocateMeClient.getInstance(LocateMeClient.class, LocateMeClientApp.context);
+        return (LocateMeClient) LocateMeClient.getInstance(LocateMeClient.class, LocateMeClientApp.context);
     }
 }
